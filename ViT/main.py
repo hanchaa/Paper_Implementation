151c03a1c214698c7c3993204d026f105a0011aa
Juhan <hanchaa@gmail.com>
2022-02-21 01:17:10 +0900

    chore(ViT): use cuda if available

--- ViT/main.py
+++ ViT/main.py
@@ -2,10 +2,14 @@ import torch
 from patch_embedding import PatchEmbedding
 
 
-x = torch.randn(8, 3, 224, 224)
-print(x.shape)
+if __name__ == "__main__":
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(device)
 
-patch_embedding = PatchEmbedding(3, 16, 768, 224)
+    x = torch.randn(8, 3, 224, 224).to(device)
+    print(x.shape)
 
-x = patch_embedding(x)
-print(x.shape)
+    patch_embedding = PatchEmbedding(3, 16, 768, 224).to(device)
+
+    x = patch_embedding(x)
+    print(x.shape)
